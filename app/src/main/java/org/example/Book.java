5b030380e749f158f1c2b8471c25b894010f63b6
Al-Fareed <alfareed@testvagrant.com>
2024-09-13 12:21:20 +0530

    added class Book to handle Books data

---
+++ app/src/main/java/org/example/Book.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Book {
+    private String title;
+    private String author;
+    private String ISBN;
+    private String genre;
+    private String publication_date;
+    private String number_of_copies;
+}
