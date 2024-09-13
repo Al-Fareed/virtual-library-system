70a4fa1ddf36331c07220c6e4775f9dda587578e
Al-Fareed <alfareed@testvagrant.com>
2024-09-13 12:31:54 +0530

    added getters and setters and constructor

--- app/src/main/java/org/example/Book.java
+++ app/src/main/java/org/example/Book.java
@@ -3,10 +3,76 @@ package org.example;
 import java.time.LocalDate;
 
 public class Book {
+    public Book() {
+    }
+
+    public Book(
+            String title,
+            String author,
+            String isbn,
+            String genre,
+            LocalDate publicationDate,
+            int numberOfCopies) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.genre = genre;
+        this.publicationDate = publicationDate;
+        this.numberOfCopies = numberOfCopies;
+    }
+
     private String title;
     private String author;
     private String isbn;
     private String genre;
     private LocalDate publicationDate;
     private int numberOfCopies;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public LocalDate getPublicationDate() {
+        return publicationDate;
+    }
+
+    public void setPublicationDate(LocalDate publicationDate) {
+        this.publicationDate = publicationDate;
+    }
+
+    public int getNumberOfCopies() {
+        return numberOfCopies;
+    }
+
+    public void setNumberOfCopies(int numberOfCopies) {
+        this.numberOfCopies = numberOfCopies;
+    }
 }
